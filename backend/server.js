d8fe6f43418a6f63ec04b66bd036152811db88e5
Hannahpohmah <111054206+Hannahpohmah@users.noreply.github.com>
2025-04-22 09:34:59 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,18 @@ dotenv.config();
 // Initialize express app
 const app = express();
 // Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 app.use(cors({
   origin: '*',
   methods: '*',
