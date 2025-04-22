2ef96193173e4811c84f9fe5639d6e42230844ed
pohmah nah-anzoh <pohmahmbuh Hannah>
2025-04-22 10:33:11 +0000

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './index.css';
-import LandingPage from './LandingPage.jsx';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import './index.css'
+import LandingPage from './LandingPage.jsx'
+import App from './App.jsx'
 
 // Simple authentication check
 const PrivateRoute = ({ children }) => {
@@ -13,19 +13,18 @@ const PrivateRoute = ({ children }) => {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename="/FleetManager_React_WebApp">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/app"
+        <Route 
+          path="/app" 
           element={
             <PrivateRoute>
               <App />
             </PrivateRoute>
-          }
+          } 
         />
       </Routes>
     </Router>
   </StrictMode>,
-);
-
+)
